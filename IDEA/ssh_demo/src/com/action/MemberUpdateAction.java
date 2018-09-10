a67d4e0f1b57cd2a4eeba5b36444c06c99b65c13
Lee979797 <35051906+Lee979797@users.noreply.github.com>
2018-09-10 14:09:21 +0800

    cal

--- IDEA/ssh_demo/src/com/action/MemberUpdateAction.java
+++ IDEA/ssh_demo/src/com/action/MemberUpdateAction.java
@@ -5,38 +5,38 @@ import com.opensymphony.xwork2.ActionSupport;
 import com.service.MemberService;
 
 public class MemberUpdateAction extends ActionSupport {
-	private MemberService memberService;
-	private Member member;
-	private long id;
+    private MemberService memberService;
+    private Member member;
+    private long id;
 
-	public void setMemberService(MemberService memberService) {
-		this.memberService = memberService;
-	}
+    public void setMemberService(MemberService memberService) {
+        this.memberService = memberService;
+    }
 
-	public Member getMember() {
-		return member;
-	}
+    public Member getMember() {
+        return member;
+    }
 
-	public void setMember(Member member) {
-		this.member = member;
-	}
+    public void setMember(Member member) {
+        this.member = member;
+    }
 
-	public long getId() {
-		return id;
-	}
+    public long getId() {
+        return id;
+    }
 
-	public void setId(long id) {
-		this.id = id;
-	}
+    public void setId(long id) {
+        this.id = id;
+    }
 
-	public String showMember() {
-		Member mb = memberService.findById(getId());
-		setMember(mb);
-		return SUCCESS;
-	}
+    public String showMember() {
+        Member mb = memberService.findById(getId());
+        setMember(mb);
+        return SUCCESS;
+    }
 
-	public String execute() {
-		memberService.update(member);
-		return SUCCESS;
-	}
+    public String execute() {
+        memberService.update(member);
+        return SUCCESS;
+    }
 }

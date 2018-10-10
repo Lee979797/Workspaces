694186759b55216650de3021af4f793f7c64af15
Lee979797 <35051906+Lee979797@users.noreply.github.com>
2018-10-10 09:25:44 +0800

    del mz_yn

--- IDEA/mz_yn/src/main/java/com/ninemax/sys/service/SysUserService.java
+++
@@ -1,4 +0,0 @@
-package com.ninemax.sys.service;
-
-public interface SysUserService {
-}
--- IDEA/mz_yn/src/main/java/com/ninemax/sys/service/impl/SysUserServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package com.ninemax.sys.service.impl;
-
-import com.ninemax.sys.service.SysUserService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class SysUserServiceImpl implements SysUserService {
-
-}
